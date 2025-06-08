75e917ea91c969ac8da0d5c7143477ac89c30cc3
artemsharkov <artemshar48@gmail.com>
2025-06-08 23:21:59 +0300

    test trud

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,18 +1,21 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
-@SpringBootTest(classes = App.class)
+@SpringBootTest(
+        classes = App.class,
+        webEnvironment = SpringBootTest.WebEnvironment.NONE
+)
+@EnableAutoConfiguration(exclude = TaskSchedulingAutoConfiguration.class)
 public class AppTest {
     @Test
     void contextLoads() {
-    }
-    @Test
-    void mainMethodShouldNotThrowException() {
-        assertThatCode(() -> App.main(new String[]{}))
-                .doesNotThrowAnyException();
     }
 }
